cd0ea7e8a1fb358477789a22bb9be94e20833f58
dandi122 <skykjk122@naver.com>
2024-08-27 23:30:36 +0900

    Feat : Home Start

---
+++ jk/src/test/java/com/web/jk/JkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.jk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
